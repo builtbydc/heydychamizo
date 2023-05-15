fe39e115b9e33ae7d2a4bbd00291e6a3c9813353
builtbydc <dcarl026@fiu.edu>
2023-05-14 18:20:11 -0600

    iridescent bubbles

--- sketch.js
+++ sketch.js
@@ -1,74 +1,33 @@
-
+let colors = [];
+let t = 0;
 
 function setup() {
+    createCanvas(windowWidth, windowHeight);
 
+    strokeWeight(3);
+    noFill();
+
+    colors.push(color(255, 173, 173));
+    colors.push(color(255, 214, 165));
+    colors.push(color(253, 255, 182));
+    colors.push(color(202, 255, 191));
+    colors.push(color(155, 246, 255));
+    colors.push(color(160, 196, 255));
+    colors.push(color(189, 178, 255));
+    colors.push(color(255, 198, 255));
+    colors.push(color(255, 255, 252));
+
+    background(0);
 }
 
 function draw() {
-    fill(255, 150, 170, 25);
-    rect(0, 0, windowWidth, windowHeight);
-    letterH.display();
-    letterE.display();
-    letterY1.display();
-    letterD.display();
-    letterY2.display();
-}
 
-class Letter {
-    constructor(letter, size, initX, initY, goalX, goalY) {
-        this.letter = letter;
-        this.size = size;
-        this.initX = initX;
-        this.initY = initY;
-        this.goalX = goalX;
-        this.goalY = goalY;
-
-        this.x = initX;
-        this.y = initY;
-        this.dx = 100*Math.random()-50;
-        this.dy = 0;
-        this.ddx = 0;
-        this.ddy = 0.1;
-        this.dd = 0.2;
-
-        this.dir = 0;
-
-        this.angle = 0;
-        this.angleSign = Math.sign(Math.random() - 0.5);
-        this.angleD = 20*Math.random()+20;
-
-        this.colorFlag = false;
+    if(t == 1) {
+        let ellipseSize = 100*Math.random();
+        stroke(colors[~~(9*Math.random())]);
+        ellipse((windowWidth - ellipseSize)*Math.random() + 0.5*ellipseSize, (windowHeight - ellipseSize)*Math.random() + 0.5*ellipseSize, ellipseSize, ellipseSize);
+        t = 0;
     }
+    t++;
 
-    update() {
-
-        if(Math.abs(this.x - this.goalX) < 1 && Math.abs(this.y - this.goalY) < 1 && Math.sqrt(this.dx*this.dx + this.dy*this.dy) < 1){
-            if(this.angle <= Math.PI / this.angleD) {
-                this.angle = 0;
-                this.colorFlag = true;
-            } else {
-                this.angle += Math.PI / this.angleD;
-                if(this.angle >= 2*Math.PI) this.angle -= 2*Math.PI;
-            }
-        } else {    
-            this.dir = Math.atan2(this.goalY - this.y, this.goalX - this.x);
-            this.ddx = this.dd*Math.cos(this.dir);
-            this.ddy = this.dd*Math.sin(this.dir);
-            this.dx += this.ddx;
-            this.dy += this.ddy;
-
-            this.dx*=0.985;
-            this.dy*=0.985;
-
-            this.x += this.dx;
-            this.y += this.dy;
-            this.angle += Math.PI / this.angleD;
-            if(this.angle >= 2*Math.PI) this.angle -= 2*Math.PI;
-        }
-
-    }
-
-    display() {
-
-    }
 }
