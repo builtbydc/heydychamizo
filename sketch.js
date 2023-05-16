34b9cfee0d535372d1719f40e3bd102d0dea267f
builtbydc <dcarl026@fiu.edu>
2023-05-16 13:03:21 -0600

    crazy painting

--- sketch.js
+++ sketch.js
@@ -17,17 +17,27 @@ function setup() {
     colors.push(color(189, 178, 255));
     colors.push(color(255, 198, 255));
     colors.push(color(255, 255, 252));
+
+    background(0);
 }
 
 function draw() {
-    background(0);
-    if(bubbles.length < 100) {
-        let size = 100*Math.random() + 25;
-        let x = (windowWidth - size)*Math.random() + 0.5*size;
-        let y = (windowHeight - size)*Math.random() + 0.5*size;
-        let color = colors[~~(9*Math.random())];
+    
+    //background(0);
 
-        let bubble = new Bubble(x, y, size, color);
+    // push();
+    // strokeWeight(5);
+    // for(let i = 0; i < 100; i += 5) {
+    //     stroke(255 - 2.55*i, 100 - i, 200 - 2*i);
+    //     if(mouseIsPressed) {
+    //         ellipse(mouseX, mouseY, i, i);
+    //     }else {
+    //         ellipse(windowWidth / 2, windowHeight / 2, i, i);
+    //     }
+    // }
+    // pop();
+    if(bubbles.length < 20) {
+        let bubble = new Bubble();
         bubbles.push(bubble);
     }
 
@@ -44,20 +54,34 @@ class Bubble {
 
     createRandom() {
         this.size = 0;
-        this.sizeGoal = 100*Math.random() + 25;
-        this.x = (windowWidth - this.size)*Math.random() + 0.5*this.size;
-        this.y = (windowHeight - this.size)*Math.random() + 0.5*this.size;
+        this.extraX = Math.max((windowWidth - windowHeight) / 2, 0);
+        this.extraY = Math.max((windowHeight - windowWidth) / 2, 0);
+        this.minSide = Math.min(windowWidth, windowHeight);
+        this.maxSize = 50;
+        this.x = this.extraX + (this.minSide - this.maxSize)*Math.random() + this.maxSize/2;
+        this.y = this.extraY + (this.minSide - this.maxSize)*Math.random() + this.maxSize/2;;
         this.color = colors[~~(9*Math.random())];
-        this.dx = 6*Math.random() + 4;
-        this.dy = 6*Math.random() + 4;
-        if(Math.random() > 0.5) this.dx *= -1;
-        if(Math.random() > 0.5) this.dy *= -1;
 
-        this.lifetime = 0;
-        this.sizeVariation = 10*Math.random() + 10;
+        if(mouseIsPressed) {
+            this.xDistance = mouseX - this.x;
+            this.yDistance = mouseY - this.y;
+        } else {
+            this.xDistance = windowWidth / 2 - this.x;
+            this.yDistance = windowHeight / 2 - this.y;
+        }
 
-        this.theta = Math.atan2(-this.y + windowHeight / 2, -this.x + windowWidth / 2);
-        this.dd = 0.1;
+        this.distance = Math.sqrt(Math.pow(this.xDistance, 2) + Math.pow(this.yDistance, 2));
+        this.sizeGoal = 25*Math.random() + 25;
+        this.theta = Math.atan2(this.yDistance, this.xDistance);
+        this.ddk = 10000;
+        this.dk = Math.sqrt(this.ddk);
+        this.d = this.dk*Math.sqrt(1/this.distance);
+        this.dSign = 1;
+        if(Math.random() > 0.5) this.dSign *= -1;
+        this.dx = this.d*Math.cos(this.theta + this.dSign * 0.5*Math.PI);
+        this.dy = this.d*Math.sin(this.theta + this.dSign *  0.5*Math.PI);
+        
+        this.dd = this.ddk/Math.pow(this.distance, 2);
         this.ddx = this.dd*Math.cos(this.theta);
         this.ddy = this.dd*Math.sin(this.theta);
     }
@@ -76,29 +100,35 @@ class Bubble {
         this.x += this.dx;
         this.y += this.dy;
 
-        this.theta = Math.atan2(-this.y + windowHeight / 2, -this.x + windowWidth / 2);
+        if(mouseIsPressed) {
+            this.xDistance = mouseX - this.x;
+            this.yDistance = mouseY - this.y;
+        } else {
+            this.xDistance = windowWidth / 2 - this.x;
+            this.yDistance = windowHeight / 2 - this.y;
+        }
+        this.distance = Math.sqrt(Math.pow(this.xDistance, 2) + Math.pow(this.yDistance, 2));
+        this.theta = Math.atan2(this.yDistance, this.xDistance);
+        this.dd = this.ddk/Math.pow(this.distance, 2);
         this.ddx = this.dd*Math.cos(this.theta);
         this.ddy = this.dd*Math.sin(this.theta);
         this.dx+=this.ddx;
         this.dy+= this.ddy;
 
+        if(this.distance < this.size / 2) this.createRandom();
         if(this.leftTheScreen()) {
             this.createRandom();
         }
-        this.lifetime++;
 
         if(this.size < this.sizeGoal) {
             this.size += 1;
-            this.lifetime = 0;
         }
     }
 
     display() {
         push();
         stroke(this.color);
-        let sizeCalculation = this.size;
-        if(this.size >= this.sizeGoal) sizeCalculation += this.sizeVariation*Math.sin(this.lifetime / 10);
-        ellipse(this.x, this.y, sizeCalculation, sizeCalculation);
+        ellipse(this.x, this.y, this.size, this.size);
         pop();
 
         this.update();
